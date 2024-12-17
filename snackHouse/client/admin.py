604862eda7b3fe02034ee241d44e00aacf6244d4
Aswin <aswin2112999@gmail.com>
2024-12-17 20:05:28 +0530

    Latest Version-Removed all existing bugs

--- snackHouse/client/admin.py
+++ snackHouse/client/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Client,Tables,Order,Snacks,Payment,Contact,Otp,BookTable
+from .models import Client,Tables,Order,Snacks,Contact,Otp,BookTable
 # Register your models here.
 admin.site.register(Client)
 admin.site.register(Tables)
 admin.site.register(Order)
 admin.site.register(Snacks)
-admin.site.register(Payment)
 admin.site.register(Contact)
 admin.site.register(Otp)
 admin.site.register(BookTable)
